fe2f90898cda3aab30f1c21cfcfa73b2b94b1f50
mabojie <634339671@qq.com>
2025-02-10 09:51:01 +0800

    feat: 自定义的404页面

---
+++ pages/[...all].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <PageNotFound />
+  </div>
+</template>
+
+<script setup>
+/***
+ * 自定义的404页面（没有能匹配的路由时会采用）
+ */
+import PageNotFound from "./pageNotFound.vue";
+
+setResponseStatus(404);
+</script>
---
+++ pages/pageNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-result
+    status="404"
+    title="404"
+    sub-title="Sorry, the page you visited does not exist."
+    class="result"
+  >
+    <template #extra>
+      <a-button type="primary" @click="backHome">Back Home</a-button>
+    </template>
+  </a-result>
+</template>
+<script setup>
+onMounted(async () => {
+  document.getElementsByTagName("body")[0].style["pointer-events"] = "auto";
+});
+
+const backHome = () => {
+  navigateTo("/");
+};
+</script>
+
+<style lang="less" scoped>
+.result {
+  padding-top: 20vh;
+}
+</style>
